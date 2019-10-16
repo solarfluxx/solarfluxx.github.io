9b59a4c831bb2d21a107ab4a9821dc582b82536b
Robert Impellitteri <kitrawserver@gmail.com>
2019-10-16 14:15:00 -0400

    Started home page

--- auth.js
+++ auth.js
@@ -1,6 +1,5 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    console.log("User is signed in");
     var displayName = user.displayName;
     var email = user.email;
     var emailVerified = user.emailVerified;
@@ -8,8 +7,17 @@ firebase.auth().onAuthStateChanged(function(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
+
+    var first_name = "";
+    var last_name = "";
+
+    firebase.database().ref('users/' + "l0yb3SGxsFajmC2LrHyJmxNClJu2").once('value').then(function(snapshot) {
+      first_name = snapshot.val().firstname;
+      last_name = snapshot.val().lastname;
+      document.getElementById("dynamic_name").innerHTML = first_name + " " + last_name;
+    });
+
   } else {
-    console.log("User is not signed in");
     if (window.location.host == "") {
       changePage("index");
     }
