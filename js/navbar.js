0f27c082860f2ffffa1e09acc9b8391c4b77fe11
Robert Impellitteri <kitrawserver@gmail.com>
2019-11-09 16:38:16 -0500

    Update 2.5

--- js/navbar.js
+++ js/navbar.js
@@ -1,60 +1,86 @@
 let root = document.documentElement;
-var togglenavbar = true;
-var animation_state = true;
-var window_state = false;
-
-var navbar = {
+var togglenavbar = true,
+animation_state = true,
+window_state = false,
+window_stage = [1090, 1480],
+navbar = {
   element: null,
   animation: {
     enable: null,
     disable: null
   },
-  compact: null,
-  expand: null,
+  size: null,
+  stage: 2,
   toggle: null
-}
+};
 
 navbar.animation.enable = function() {
   root.style.setProperty('--navbar-transition', root.style.getPropertyValue('--navbar-transition-on'));
-}
+};
 
 navbar.animation.disable = function() {
   root.style.setProperty('--navbar-transition', "unset");
-}
+};
 
-navbar.compact = function(full) {
-  if (full) root.style.setProperty('--navbar-width', 0); else root.style.setProperty('--navbar-width', "68px");
-  togglenavbar = false;
-}
-
-navbar.expand = function(full) {
-  if (full) root.style.setProperty('--navbar-width', "256px"); else root.style.setProperty('--navbar-width', "68px");
-  togglenavbar = true;
-}
+navbar.size = function(stage) {
+  switch (stage) {
+    case 0:
+      root.style.setProperty('--navbar-width', "0px");
+      root.style.setProperty('--navbar-true-width', "0px");
+      navbar.stage = 0;
+      break;
+    case 1:
+      root.style.setProperty('--navbar-width', "68px");
+      root.style.setProperty('--navbar-true-width', "68px");
+      root.style.setProperty('--sidenav-padding-right', root.style.getPropertyValue('--sidenav-padding-right-on'));
+      root.style.setProperty('--sidenav-padding-left', root.style.getPropertyValue('--sidenav-padding-left-on'));
+      root.style.setProperty('--sidenav-navtext-opacity', "0");
+      navbar.stage = 1;
+      break;
+    case 2:
+      root.style.setProperty('--navbar-width', "256px");
+      root.style.setProperty('--navbar-true-width', "256px");
+      root.style.setProperty('--sidenav-padding-right', root.style.getPropertyValue('--sidenav-padding-off'));
+      root.style.setProperty('--sidenav-padding-left', root.style.getPropertyValue('--sidenav-padding-off'));
+      root.style.setProperty('--sidenav-navtext-opacity', "1");
+      navbar.stage = 2;
+      break;
+    case 3:
+      root.style.setProperty('--navbar-true-width', "256px");
+      root.style.setProperty('--sidenav-padding-right', root.style.getPropertyValue('--sidenav-padding-off'));
+      root.style.setProperty('--sidenav-padding-left', root.style.getPropertyValue('--sidenav-padding-off'));
+      root.style.setProperty('--sidenav-navtext-opacity', "1");
+      navbar.stage = 2;
+      break;
+  }
+};
 
 navbar.toggle = function() {
-  if (togglenavbar) {
-    navbar.compact(false);
+  navbar.animation.enable();
+  if ($(window).width() > window_stage[1]) {
+    if (navbar.stage == 2) navbar.size(1); else if (navbar.stage == 1) navbar.size(2);
   } else {
-    navbar.expand(true);
+    if (navbar.stage == 0) navbar.size(3); else if (navbar.stage == 3) navbar.size(0);
   }
-}
+};
 
-if ($(window).width() < 950) {
-  navbar.animation.disable();
-  navbar.compact(true);
-  window_state = true;
-}
+navbarResize(false);
 
 $(window).resize(function() {
-  if ($(window).width() < 950 && window_state == false) {
-    navbar.animation.disable();
-    navbar.compact(true);
-    window_state = true;
-  }
-  if ($(window).width() > 950 && window_state == true) {
-    navbar.animation.disable();
-    navbar.expand(true);
-    window_state = false;
-  }
+  navbarResize(true);
 });
+
+function navbarResize(animation) {
+  if (animation) {
+    navbar.animation.enable();
+  } else {
+    navbar.animation.disable();
+  }
+  if ($(window).width() < window_stage[0]) {
+    navbar.size(0);
+  } else if ($(window).width() < window_stage[1]) {
+    navbar.size(1);
+  } else {
+    navbar.size(2);
+  }
+}
