98c7904fd1687c9b99202181d986c403e4981d69
Robert Impellitteri <kitrawserver@gmail.com>
2020-01-01 18:16:51 -0500

    Update 1.5 Added firebase hook

---
+++ other/desist/database.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDF9ywIozLDs93wRdpLtl5zXRwxgph4NX4",
+  authDomain: "projects-9cfe6.firebaseapp.com",
+  databaseURL: "https://projects-9cfe6.firebaseio.com",
+  projectId: "projects-9cfe6",
+  storageBucket: "projects-9cfe6.appspot.com",
+  messagingSenderId: "660559074565",
+  appId: "1:660559074565:web:b1fdd65df01684ff26536c",
+  measurementId: "G-KTQ3P6H6FX"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var database = firebase.database();
--- other/desist/line_creator.js
+++ other/desist/line_creator.js
@@ -24,22 +24,30 @@ function Ripple(event) {
   }, 415);
 }
 
-function loadInputs() {
+function loadInputs(input) {
   var inputs = $("dl-input");
 
-  inputs.each(function(index) {
-    if (inputs[index].querySelector("dl-hint") == null) {
-      if (inputs[index].querySelector("dl-select") == null) var html_input = document.createElement("input");
+  if (input == null || input == undefined) {
+    inputs.each(function(index) {
+      loadInput(inputs[index]);
+    });
+  } else {
+    loadInput(input);
+  }
+
+  function loadInput(input) {
+    if (input.querySelector("dl-hint") == null) {
+      if (input.querySelector("dl-select") == null) var html_input = document.createElement("input");
       var hint_element = document.createElement("dl-hint");
 
-      if (inputs[index].getAttribute("hint") != null) {
-        hint_element.innerHTML = inputs[index].getAttribute("hint");
-        inputs[index].prepend(hint_element);
+      if (input.getAttribute("hint") != null) {
+        hint_element.innerHTML = input.getAttribute("hint");
+        input.prepend(hint_element);
       }
 
-      if (inputs[index].querySelector("dl-select") == null) {
-        html_input.setAttribute("type", inputs[index].getAttribute("type"));
-        if (inputs[index].getAttribute("disabled") != null) html_input.setAttribute("disabled", "");
+      if (input.querySelector("dl-select") == null) {
+        html_input.setAttribute("type", input.getAttribute("type"));
+        if (input.getAttribute("disabled") != null) html_input.setAttribute("disabled", "");
         html_input.addEventListener('focus', (event) => {
           event.target.parentElement.classList.add("open");
           event.target.parentElement.classList.add("focus");
@@ -48,14 +56,14 @@ function loadInputs() {
           if (event.target.value == "") event.target.parentElement.classList.remove("open");
           event.target.parentElement.classList.remove("focus");
         });
-        inputs[index].append(html_input);
+        input.append(html_input);
       } else {
-        inputs[index].classList.add("select");
+        input.classList.add("select");
       }
 
-      if (inputs[index].querySelector("dl-select") == null && html_input.value != "") inputs[index].classList.add("open");
+      if (input.querySelector("dl-select") == null && html_input.value != "") input.classList.add("open");
     }
-  });
+  }
 }
 loadInputs();
 
@@ -125,103 +133,182 @@ buttons.forEach(function(button) {
   });
 });
 
-let generated_code = [];
+let lines = [];
 let selector = "@a";
+let names = [
+  "Chris",
+  "Bree",
+  "RAI",
+  "Computer",
+  "Custom",
+];
+
+function Line() {
+  this.name = function(name) {
+    if (name != null && name != undefined) select_name.setValue(name);
+    return this.select.getAttribute("selection");
+  }
+
+  this.sentence = function(sentence) {
+    if (sentence != null && sentence != undefined) this.input_sentence.setValue(sentence);
+    return this.input_sentence.querySelector("input").value;
+  }
+
+
+  this.element = document.createElement("dl-editor-item");
+  this.input_sentence = document.createElement("dl-input");
+
+  let select_name = document.createElement("dl-input");
+  let button_remove = document.createElement("dl-remove");
+  let item_handle = document.createElement("dl-handle");
+
+  let handle_icon = document.createElement("i");
+  let remove_icon = document.createElement("i");
+  handle_icon.classList.add("material-icons");
+  remove_icon.classList.add("material-icons");
+  handle_icon.innerHTML = 'unfold_more';
+  remove_icon.innerHTML = 'remove';
+
+  item_handle.appendChild(handle_icon);
+  button_remove.appendChild(remove_icon);
+  button_remove.setAttribute("onclick", "line_generator.item.remove(this);");
+
+  select_name.setAttribute("field", "name");
+  select_name.setAttribute("hint", "Name");
+
+  let select_menu = document.createElement("dl-select-menu");
+  let option_chris = document.createElement("dl-option");
+  let option_bree = document.createElement("dl-option");
+  let option_rai = document.createElement("dl-option");
+  let option_custom = document.createElement("dl-option");
+
+  option_chris.setAttribute("text", "Chris");
+  option_bree.setAttribute("text", "Bree");
+  option_rai.setAttribute("text", "RAI");
+  option_custom.setAttribute("text", "Custom");
+
+  select_menu.appendChild(option_chris);
+  select_menu.appendChild(option_bree);
+  select_menu.appendChild(option_rai);
+  select_menu.appendChild(option_custom);
+
+  this.select = document.createElement("dl-select");
+  select_name.appendChild(this.select);
+  select_name.appendChild(document.createElement("dl-select-overlay"));
+  select_name.appendChild(select_menu);
+
+  this.input_sentence.setAttribute("field", "sentence");
+  this.input_sentence.setAttribute("hint", "Sentence");
+  this.input_sentence.setAttribute("type", "text");
+
+  loadInputs(select_name);
+  loadInputs(this.input_sentence);
+
+  this.element.appendChild(item_handle);
+  this.element.appendChild(select_name);
+  this.element.appendChild(this.input_sentence);
+  this.element.appendChild(button_remove);
+}
+
+var chaptersRef = firebase.database().ref('desist/line_generator/chapters/');
+
+HTMLElement.prototype.setValue = function(value) {
+  var type;
+  if (this.querySelector("dl-select") != null) type = "select";
+
+  if (type == "select") {
+    this.querySelector("dl-select").setAttribute("selection", value);
+  } else {
+    this.querySelector("input").value = value;
+  }
+
+  this.classList.add("open");
+}
+
+HTMLElement.prototype.getValue = function() {
+  var type;
+  if (this.querySelector("dl-select") != null) type = "select";
+
+  if (type == "select") {
+    let value = this.querySelector("dl-select").getAttribute("selection");
+    if (value == null) value = "";
+    return value;
+  } else {
+    return this.querySelector("input").value;
+  }
+}
 
 let line_generator = {
   generate: function() {
     document.querySelector("dl-code").innerHTML = "";
+    let time = 0;
     packery.getItemElements().forEach(function(item) {
-      let name = item.querySelector('[field="name"]').querySelector("dl-select").getAttribute("selection");
-      let sentence = item.querySelector('[field="sentence"]').querySelector("input").value;
-
+      let name = item.querySelector('[field="name"]').getValue();
+      let sentence = item.querySelector('[field="sentence"]').getValue();
       let code_item = document.createElement("dl-code-item");
-      code_item.innerHTML = 'tellraw '+selector+' {"rawtext":[{"translate":"<%%1> %%2", "with":["'+name+'", "'+sentence+'"]}]}';
+
+      code_item.innerHTML = 'execute @p[scores={timer='+time+'}] ~ ~ ~ tellraw '+selector+' {"rawtext":[{"translate":"<%%1> %%2", "with":["'+name+'", "'+sentence+'"]}]}';
+      time = time + Math.round((20 *((((sentence.length / 16) + ((sentence.match(/,/g) || []).length) * 0.4) + 0.2))));
 
       document.querySelector("dl-code").append(code_item);
     });
   },
   item: {
-    create: function() {
-      let item_container = document.createElement("dl-editor-item");
-      let select_name = document.createElement("dl-input");
-      let input_sentence = document.createElement("dl-input");
-      let button_remove = document.createElement("dl-remove");
-      let item_handle = document.createElement("dl-handle");
+    add: function(name, sentence) {
+      let line = new Line();
+      lines.push(line);
 
-      let handle_icon = document.createElement("i");
-      let remove_icon = document.createElement("i");
-      handle_icon.classList.add("material-icons");
-      remove_icon.classList.add("material-icons");
-      handle_icon.innerHTML = 'unfold_more';
-      remove_icon.innerHTML = 'remove';
+      if (name != null && name != undefined) line.name(name);
+      if (sentence != null && sentence != undefined) line.sentence(sentence);
 
-      item_handle.appendChild(handle_icon);
-      button_remove.appendChild(remove_icon);
-      button_remove.setAttribute("onclick", "line_generator.item.remove(this);");
-
-      // button_remove.innerHTML = "Remove";
-      // button_remove.setAttribute("onclick", "line_generator.item.remove(this);");
-
-      select_name.setAttribute("field", "name");
-      select_name.setAttribute("hint", "Name");
-
-      let select_menu = document.createElement("dl-select-menu");
-      let option_chris = document.createElement("dl-option");
-      let option_bree = document.createElement("dl-option");
-      let option_rai = document.createElement("dl-option");
-      let option_custom = document.createElement("dl-option");
-
-      option_chris.setAttribute("text", "Chris");
-      option_bree.setAttribute("text", "Bree");
-      option_rai.setAttribute("text", "RAI");
-      option_custom.setAttribute("text", "Custom");
-
-      select_menu.appendChild(option_chris);
-      select_menu.appendChild(option_bree);
-      select_menu.appendChild(option_rai);
-      select_menu.appendChild(option_custom);
-
-      select_name.appendChild(document.createElement("dl-select"));
-      select_name.appendChild(document.createElement("dl-select-overlay"));
-      select_name.appendChild(select_menu);
-
-      input_sentence.setAttribute("field", "sentence");
-      input_sentence.setAttribute("hint", "Sentence");
-      input_sentence.setAttribute("type", "text");
-
-      item_container.appendChild(item_handle);
-      item_container.appendChild(select_name);
-      item_container.appendChild(input_sentence);
-      item_container.appendChild(button_remove);
-      return item_container;
-    },
-    add: function() {
-      let element = this.create();
+      let element = line.element;
       document.querySelector("dl-editor").append(element);
       selects.load();
-      loadInputs();
       var draggie = new Draggabilly(element, {
         axis: 'y',
         handle: 'dl-handle',
         containment: 'dl-editor'
       });
 
-      packery.bindDraggabillyEvents(draggie);
-      packery.appended(element);
+      if (packery != null) {
+        packery.bindDraggabillyEvents(draggie);
+        packery.appended(element);
+        packery.shiftLayout();
+      }
     },
     remove: function(element) {
+      lines.splice($(element.parentElement).index(), 1);
       packery.remove(element.parentElement);
-      // element.parentElement.remove();
       packery.shiftLayout();
+    }
+  },
+  upload: function() {
+    var chapter_number = prompt("Enter the chapter number. WARNING: If you put a number in of a chapter that already exists it will be overridden.", 0);
+    let items = [];
+
+    packery.getItemElements().forEach(function(item) {
+      let name = item.querySelector('[field="name"]').getValue();
+      let sentence = item.querySelector('[field="sentence"]').getValue();
+
+      items.push({
+        name: name,
+        sentence: sentence
+      });
+    });
+
+    if (chapter_number != null) {
+      chapter_number--;
+      chaptersRef.child(chapter_number).set(items);
     }
   }
 };
 
+line_generator.item.add();
+
 var drag_container = document.querySelector('dl-editor');
 var drag_item_size = getSize(document.querySelector('dl-editor-item'));
 var packery = new Packery(drag_container, {
-  rowHeight: drag_item_size.outerHeight
+  rowHeight: -56
 });
 
 var drag_items = packery.getItemElements();
@@ -235,3 +322,27 @@ for (var i=0, len = drag_items.length; i < len; i++) {
 
   packery.bindDraggabillyEvents(draggie);
 }
+
+chaptersRef.on('value', function(snapshot) {
+  document.querySelector("dl-chapters").innerHTML = "";
+  document.querySelector("dl-loader-container").classList.add("show");
+  snapshot.forEach(function(childSnapshot) {
+    let chapter_text = document.createElement("dl-chapter-item");
+    chapter_text.setAttribute("ref", childSnapshot.key);
+    chapter_text.innerHTML = "Chapter " + (parseInt(childSnapshot.key)+1);
+    document.querySelector("dl-chapters").appendChild(chapter_text);
+    document.querySelector("dl-loader-container").classList.remove("show");
+
+    chapter_text.addEventListener("click", (event) => {
+      document.querySelector("dl-loader-container").classList.add("show");
+
+      chaptersRef.child(event.target.getAttribute("ref")).once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          line_generator.item.add(childSnapshot.val().name, childSnapshot.val().sentence);
+        });
+      }).then(function() {
+          document.querySelector("dl-loader-container").classList.remove("show");
+      });
+    });
+  });
+});
